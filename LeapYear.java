cec86f3c6abe75862e6650dbf64fbb8437827c9c
subham-swain-01 <swainsubham131@gmail.com>
2025-06-23 08:37:24 +0530

    Update LeapYear.java

--- LeapYear.java
+++ LeapYear.java
@@ -6,7 +6,6 @@ public class LeapYear {
         System.out.print("Enter a year: ");
         int year = sc.nextInt();
 
-        // Leap year conditions
         if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
             System.out.println(year + " is a Leap Year.");
         } else {
